393338925c9b1ab6bc542493add66d2ea063084f
karis Nimmo (student) <2263984N@student.gla.ac.uk>
2020-02-14 16:42:01 +0000

    chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug':('name',)}
@@ -9,3 +10,4 @@ class PageAdmin(admin.ModelAdmin):
 	list_display = ('title','category','url')
 admin.site.register(Page, PageAdmin)
 
+admin.site.register(UserProfile)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'rango:login'
